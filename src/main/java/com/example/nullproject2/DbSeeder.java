db203024dd8f37600cac363c549ec0560109c72d
MariosGen <mariosgen@athtech.gr>
2021-05-06 20:11:21 +0300

    minor fixes

--- src/main/java/com/example/nullproject2/DbSeeder.java
+++ src/main/java/com/example/nullproject2/DbSeeder.java
@@ -36,17 +36,5 @@ public class DbSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Faker faker = new Faker();
-
-//        for (int i = 0 ; i < 4 ; i++){
-//            User user1 = new User();
-//            user1.setName(faker.medical().hospitalName());
-//            user1.setAddress(faker.address().streetAddress());
-//            user1.setPhone_number(faker.phoneNumber().phoneNumber());
-//            user1.setCity(faker.address().cityName());
-//            user1.setCountry(faker.address().country());
-//            user1.setAvailable_doses(25);
-//            user1.set
-//        }
     }
 }
