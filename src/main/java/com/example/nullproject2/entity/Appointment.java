9c260bec477ff16f788113d99bc1c6e30065d7be
MariosGen <mariosgen@athtech.gr>
2021-05-20 12:20:40 +0300

    small changes

--- src/main/java/com/example/nullproject2/entity/Appointment.java
+++ src/main/java/com/example/nullproject2/entity/Appointment.java
@@ -1,24 +1,22 @@
 package com.example.nullproject2.entity;
 
-import com.example.nullproject2.enumerations.Brand;
-import com.example.nullproject2.enumerations.PatientStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+
 
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
-public class BigChain {
+public class Appointment {
 
     @Id
     private String id;
 
     private User hospital;
+
     private Patient patient;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
